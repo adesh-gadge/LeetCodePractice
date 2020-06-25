372fea1d0329d65f46a9dad78bcee4df378252e9
Adesh Gadge <adesh.gadge@gmail.com>
2020-06-25 11:57:23 -0400

    Create ReverseWordsInAString.py
    reversing words in a string

---
+++ ReverseWordsInAString.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans=''
+        words=s.split()
+        
+        for i in range(len(words)):
+            ans+=words[len(words)-1-i]
+            if i!=len(words)-1:
+                ans+=' '
+        return ans    
