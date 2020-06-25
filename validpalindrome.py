1db248b2922761d48757e6b76320d7e2f8ad72f9
Adesh Gadge <adesh.gadge@gmail.com>
2020-06-25 09:48:39 -0400

    Update validpalindrome.py
    isalnum does not work always hence replaced with regex

--- validpalindrome.py
+++ validpalindrome.py
@@ -1,18 +1,18 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        x=''
-        
+    def isPalindrome(self, s: str) -> bool:        
+        x=""
         for i in range(len(s)):
             if s[i].isalnum():
                 x+=s[i]
-        siz=len(x)  
         x=x.lower()
-        if siz==0:
-            return True
-        for i in range(int(siz/2)):
-            if x[i]!=x[siz-(i+1)]:
+        x = re.sub('[^a-z0-9]','',x)
+        for i in range(int(len(x)/2)):
+            if x[i]!=x[len(x)-i-1]:
                 return False
-        
-        return True
+        return True    
+    
+    
+    
+    
             
             
