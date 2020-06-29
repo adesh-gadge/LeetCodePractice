2c024b8adfa09e9ee7914ffc1dec36ac2b543791
Adesh Gadge <adesh.gadge@gmail.com>
2020-06-29 13:19:08 -0400

    Update groupAnarams.py
    outputs removed

--- groupAnarams.py
+++ groupAnarams.py
@@ -9,19 +9,16 @@ class Solution(object):
         j=0
         for i in strs:
             key=''.join(sorted(i))
-            print('key:',key)
             if key not in dicts:
                 dicts[key]= j
-                print(dicts)
+                
                 out.append([i])
                 j+=1
-                print('out ',out)
+                
                 
             else:
                 out[dicts[key]].append(i)
-                print('out ',out)
-        print(dicts)        
+                
+        
         return out
                 
-            
-            
