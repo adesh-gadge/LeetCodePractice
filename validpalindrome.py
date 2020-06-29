f193d19ae7069acfe0cd398d806b31c51b6a1570
Adesh Gadge <adesh.gadge@gmail.com>
2020-06-29 12:27:03 -0400

    Update validpalindrome.py
    lesser space solution

--- validpalindrome.py
+++ validpalindrome.py
@@ -1,17 +1,17 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:        
-        x=""
-        for i in range(len(s)):
-            if s[i].isalnum():
-                x+=s[i]
-        x=x.lower()
-        x = re.sub('[^a-z0-9]','',x)
-        for i in range(int(len(x)/2)):
-            if x[i]!=x[len(x)-i-1]:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=s.lower()
+        s = re.sub('[^a-z0-9]','',s)
+        for i in range(int(len(s)/2)):
+            if s[i]!=s[len(s)-i-1]:
                 return False
-        return True    
-    
-    
+        return True
+        
+
     
     
             
