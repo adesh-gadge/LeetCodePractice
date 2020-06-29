ad6444bbf193d698dafa085fd699438efbeff64f
Adesh Gadge <adesh.gadge@gmail.com>
2020-06-29 12:54:35 -0400

    Update longestPalanidromicSubstring.py
    Faster Solution

--- longestPalanidromicSubstring.py
+++ longestPalanidromicSubstring.py
@@ -1,27 +1,17 @@
 class Solution(object):
-    def validPalin(self,s):
-        x= s.lower()
-        x=re.sub('[^a-z0-9]','',x)
-        for i in range(len(x)/2):
-            if x[i]!=x[len(x)-1-i]:
-                return False
-        return True 
-    
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        maxPalin=0
-        out=''
+        m=''
         for i in range(len(s)):
-            for j in range(i,len(s)):
-                if self.validPalin(s[i:j+1]) and len(s[i:j+1])>maxPalin:
-                    out=s[i:j+1]
-                    maxPalin=len(s[i:j+1])
-        return out            
+            for j in range(len(s),i,-1):
+                if len(m)> j-i:
+                    break
+                elif s[i:j]==s[i:j][::-1]:
+                    m=s[i:j]
+        return m            
                     
                     
-                    
-                
-                
+        
